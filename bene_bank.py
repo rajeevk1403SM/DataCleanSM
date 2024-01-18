5b3dfb7cfa04992282267177b3820c42c5b4f28d
rajeevk1403SM <156788064+rajeevk1403SM@users.noreply.github.com>
2024-01-18 14:56:48 +0100

    Update bene_bank.py

--- bene_bank.py
+++ bene_bank.py
@@ -4,7 +4,7 @@ import streamlit as st
 def populate_bene_bank_location(df, key_df):
     try:
         # Merge the main DataFrame with the key DataFrame, keeping all columns from the main DataFrame
-        merged_df = df.merge(key_df, left_on='Buy Currency', right_on='Currency', how='left')
+        merged_df = df.merge(key_df, left_on='Client Buy CCY', right_on='Currency', how='left')
 
         # Update the Bene Bank Location with the Country from the key DataFrame
         df['Bene Bank Location'] = merged_df['Country']
